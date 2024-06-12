d6551a1c20bac45875984db216223a7932416a25
Mokni Hamdi <86926622+Mk-1000@users.noreply.github.com>
2024-06-12 09:58:53 +0100

    Nchlh lyoum

--- migrations/Version20240606111940.php
+++ migrations/Version20240606111940.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240504231404 extends AbstractMigration
+final class Version20240606111940 extends AbstractMigration
 {
     public function getDescription(): string
     {
